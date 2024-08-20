a274aca6409da90dfc50207ce187b68f5a54d230
GRAZIELLA ALVES DA SILVA <alvesgraziella9@gmail.com>
2024-08-20 20:09:57 -0300

    Create exerc12.py

---
+++ exerc12.py
@@ -0,0 +1,8 @@
+print("\33[1;35;40mPositivo ou Negativo?\33[m\n")
+
+numero = int(input("Digite um número: "))
+
+if(numero>=0):
+    print("Este número é \33[1;35;40mPOSITIVO!\33[m")
+else:
+    print("Este número é \33[1;35;40mNEGATIVO!\33[m")

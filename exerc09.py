90972242065e0dc63c835cb539b1f937e6ee664b
GRAZIELLA ALVES DA SILVA <alvesgraziella9@gmail.com>
2024-08-20 18:52:38 -0300

    Create exerc09.py

---
+++ exerc09.py
@@ -0,0 +1,6 @@
+numero = int(input("\33[1;35;40mInforme um número: \33[m"))
+
+if(numero%5==0):
+    print("Esse número é multiplo de 5!")
+else:
+    print("Esse número \33[1;31;40mnão\33[m é multiplo de 5!")    

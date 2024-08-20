4a68801bf5ebd151bf45eaa2e92806ade5171b60
GRAZIELLA ALVES DA SILVA <alvesgraziella9@gmail.com>
2024-08-20 20:13:52 -0300

    Create exerc13.py

---
+++ exerc13.py
@@ -0,0 +1,6 @@
+print("\33[1;35;40mConversor de metros para centímetros.\33[m\n")
+
+metros = float(input("Informe a medida em metros: "))
+centimetros = metros * 100
+
+print(f"A medida em centímetros é: ",centimetros)

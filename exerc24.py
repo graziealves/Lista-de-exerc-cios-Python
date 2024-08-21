26e7fe343eb2c3df62a70562649baa1c9fdda2fa
GRAZIELLA ALVES DA SILVA <alvesgraziella9@gmail.com>
2024-08-21 20:22:26 -0300

    Update exerc24.py

--- exerc24.py
+++ exerc24.py
@@ -4,10 +4,8 @@ altura = float(input("Informe o valor da altura do quadrado: "))
 base = float(input("Informe o valor da base do quadrado:  "))
 
 if (altura != base ):
-   print(" \33 1;31;40mEsta área não é de um quadrado!\33[m")
+   print(" \33[1;31;40mEsta área não é de um quadrado!\33[m")
+else:
+  area = base * altura
 
-area = base * altura
-
-
-
-print("\33[1;35;40m A área do quadrado é:\33[m", area )
+print("\33[1;35;40mA área do quadrado é:\33[m", area )

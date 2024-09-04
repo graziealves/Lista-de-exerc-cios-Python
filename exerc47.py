756354e36a270437a534937a2fa60cb050b4f9c0
GRAZIELLA ALVES DA SILVA <alvesgraziella9@gmail.com>
2024-09-04 20:52:45 -0300

    Create exerc47.py

---
+++ exerc47.py
@@ -0,0 +1,7 @@
+print("\33[1;35;40mExpoente de um número.\33[m \n")
+
+numero = int(input("\33[1;35;40mDigite um número: \33[m"))
+expoente = int(input("\33[1;35;40mDigite o segundo numero: \33[m"))
+
+print(numero**expoente)
+    
